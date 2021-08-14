6758ce58f719fa2d416ac22fba86b6b86226fca4
Jakezo <cakim21@naver.com>
2021-08-14 22:51:22 +0900

    2021-08-14

--- src/study/Study_IndexOf_01.java
+++ src/study/Study_IndexOf_01.java
@@ -9,6 +9,7 @@ public class Study_IndexOf_01 {
 			 System.out.println( indexOfTestOne.indexOf("o",5) ); // 7 
 			 System.out.println( indexOfTestTwo.indexOf("o") ); //
 			 System.out.println( indexOfTestTwo.indexOf("el") ); //
+			 
 			 System.out.println( indexOfTestOne.lastIndexOf("o") ); // 7 
 			 System.out.println( indexOfTestOne.lastIndexOf("x") ); // -1 
 			 System.out.println( indexOfTestOne.lastIndexOf("o",5) ); // 
---
+++ src/study/array_2.java
@@ -0,0 +1,27 @@
+package study;
+
+import java.util.Scanner;
+
+public class array_2 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int [][] a = {
+				{1,2},
+				{7,8},
+				{5,6}
+		};
+		
+		// 2차원 배열의 접근(순회) 방식은 "행 우선(row major)"
+		
+		for(int i = 0; i< a.length; i++) {
+			for(int j=0; j< a[i].length; j++) {
+				System.out.print(a[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+}

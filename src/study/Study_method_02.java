6f7e0fae7aa579bb4481753e4aafc09e0e5fb574
Jakezo <cakim21@naver.com>
2021-09-05 22:01:09 +0900

    2021-09-05

---
+++ src/study/Study_method_02.java
@@ -0,0 +1,25 @@
+package study;
+
+class CallMeName{
+	static void print() {
+		System.out.println("프린트를 합니다");
+	}
+	void print2() {
+		System.out.println("나는 이순신입니다");
+	}
+}
+
+
+public class Study_method_02 {
+	
+		public static void main(String[] args) {
+		
+			CallMeName.print();//인스턴스를 생성하지 않아도 호출이 가능
+			
+			CallMeName cal = new CallMeName();
+			
+			cal.print2(); //인스턴스를 생성하여야만 호출이 가능
+		
+	}
+
+}

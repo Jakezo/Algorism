7d7811a1110099969e1719aaff551cac45cc89b9
Jakezo <cakim21@naver.com>
2021-09-13 23:02:06 +0900

    2021-09-13

--- src/study/Strudy_Method_03.java
+++ src/study/Strudy_Method_03.java
@@ -7,13 +7,15 @@ class Carino{
 	String color = "검정";
 	int maxSpeed = 350;
 	int speed;
-	
+
 	Carino(String color, int speed){
 		this.color = color;
 		this.speed =speed;
 		
 	}
  }
+
+
 
 public class Strudy_Method_03 {
 	public static void main(String[] args) {
@@ -26,8 +28,6 @@ public class Strudy_Method_03 {
 		 
 		 System.out.println("속도" + ca.speed); 
 		 
-		 
-		
 		
 	}
 
